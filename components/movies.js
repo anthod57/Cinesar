ba2f2e972ab0616593f6b818c46564f39204660c
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-04-13 20:56:02 +0200

    Fixing fetching

--- components/movies.js
+++ components/movies.js
@@ -8,7 +8,7 @@ export const Movies = (props) => {
             <Slider onClick={() => { updateSlider() }}>
                 <h2>{props.title}</h2>
                 <SliderContainer>
-                    {props.data?.results.map((movie, index) => {
+                    {props.data ? props.data?.results.map((movie, index) => {
                         return (
                             <Slide key={index}>
                                 <div className="picture">
@@ -16,16 +16,16 @@ export const Movies = (props) => {
                                 </div>
                             </Slide>
                         )
-                    })}
-                    {props.data?.results.map((movie, index) => {
+                    }) : ""}
+                    {props.data ? props.data?.results.map((movie, index) => {
                         return (
                             <Slide key={index}>
                                 <div className="picture">
-                                    <Image quality={100} layout='fill' objectFit='cover' src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} loading="lazy"/>
+                                    <Image quality={100} layout='fill' objectFit='cover' src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} priority />
                                 </div>
                             </Slide>
                         )
-                    })}
+                    }) : ""}
                 </SliderContainer>
             </Slider>
         </>
