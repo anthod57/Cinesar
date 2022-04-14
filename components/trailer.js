73fd19b2312c084366ac2ac39e863eadf9fe4295
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-04-14 17:47:08 +0200

    Fixed 401 error

--- components/trailer.js
+++ components/trailer.js
@@ -13,7 +13,7 @@ export const Trailer = (data) => {
         <>
             <Container id={data.id} style={{display: "none"}} ref={trailerContainer} onClick={() => {trailerContainer.current.style = "display: none;"}}>
                 <Wrapper>
-                    <iframe id={"trailer-iframe"} src={`https://www.youtube.com/embed/${data.videoKey}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <iframe id={"trailer-iframe"} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 </Wrapper>
             </Container>
         </>
