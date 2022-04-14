a4e767fbad80ba212b80a3842f6103c50cc8bbae
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-04-14 13:41:09 +0200

    Test without placeholders

--- components/movies.js
+++ components/movies.js
@@ -4,14 +4,8 @@ import { Slider, SliderContainer, Slide } from '../styles/StyledMovies';
 
 export const Movies = (props) => {
 
-    const placeHolder = React.createRef();
-    const container = React.createRef();
-
     useEffect(() => {
-        if (props.data) {
-            placeHolder.current.remove();
-            container.current.style = "";
-        }
+
     }, [])
 
 
@@ -19,34 +13,7 @@ export const Movies = (props) => {
         <>
             <Slider onClick={() => { updateSlider() }}>
                 <h2>{props.title}</h2>
-                <SliderContainer ref={placeHolder} animate={false}>
-                    <Slide>
-                        <div className="picture">
-                            <Image layout='fill' objectFit='cover' src={"/images/placeholder.jpg"} priority />
-                        </div>
-                    </Slide>
-                    <Slide>
-                        <div className="picture">
-                            <Image layout='fill' objectFit='cover' src={"/images/placeholder.jpg"} priority />
-                        </div>
-                    </Slide>
-                    <Slide>
-                        <div className="picture">
-                            <Image layout='fill' objectFit='cover' src={"/images/placeholder.jpg"} priority />
-                        </div>
-                    </Slide>
-                    <Slide>
-                        <div className="picture">
-                            <Image layout='fill' objectFit='cover' src={"/images/placeholder.jpg"} priority />
-                        </div>
-                    </Slide>   
-                    <Slide>
-                        <div className="picture">
-                            <Image layout='fill' objectFit='cover' src={"/images/placeholder.jpg"} priority />
-                        </div>
-                    </Slide>      
-                </SliderContainer>
-                <SliderContainer ref={container} style={{display: "none"}}>
+                <SliderContainer>
                     {props.data.results ? props.data.results?.map((movie, index) => {
                         return (
                             <Slide key={index}>
