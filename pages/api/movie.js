45906a33452af5e729a92fcf5b9d9a706754aa91
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-04-15 15:43:37 +0200

    removed cors

--- pages/api/movie.js
+++ pages/api/movie.js
@@ -1,26 +1,8 @@
 import { server } from "../../config";
 import axios from "axios";
-import Cors from 'cors'
-
-const cors = Cors({
-    methods: ['GET', 'HEAD'],
-})
-
-function runMiddleware(req, res, fn) {
-    return new Promise((resolve, reject) => {
-        fn(req, res, (result) => {
-            if (result instanceof Error) {
-                return reject(result)
-            }
-
-            return resolve(result)
-        })
-    })
-}
 
 export default async function handler(req, res) {
-    await runMiddleware(req, res, cors);
-    
+
     if(!req.query.id) res.status(500).send();
     await axios(`${server}/${req.query.id}?api_key=${process.env.API_KEY}&language=fr-FR&page=1`).then( (response) => {
         res.status(200).json(response.data);
--- pages/api/trailer.js
+++ pages/api/trailer.js
@@ -1,25 +1,8 @@
 import { server } from "../../config";
 import axios from "axios";
-import Cors from 'cors'
-
-const cors = Cors({
-    methods: ['GET', 'HEAD'],
-})
-
-function runMiddleware(req, res, fn) {
-    return new Promise((resolve, reject) => {
-        fn(req, res, (result) => {
-            if (result instanceof Error) {
-                return reject(result)
-            }
-
-            return resolve(result)
-        })
-    })
-}
 
 export default async function handler(req, res) {
-    await runMiddleware(req, res, cors);
+
   await axios.get(`${server}/${req.query.id}/videos?api_key=${process.env.API_KEY}&language=fr-FR`).then((response) => {
     res.status(200).json(response.data.results[0].key);
   }).catch((error) => {
